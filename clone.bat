41ae714bcc41723f3c8d02cc1c28e86d6efe0aab
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-24 16:04:02 +0300

    added git clone commands to clone.bat

---
+++ clone.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ..
+git clone https://github.com/vilppes/accessibility-resources
+git clone https://github.com/vilppes/notes-on-learning-bluetooth
+git clone https://github.com/vilppes/emergency-messaging --branch dev-sm emergency-messaging-devsm
