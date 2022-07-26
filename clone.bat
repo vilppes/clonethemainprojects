b29b94457e514d15de0ce240d2979b5431ebdb37
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-26 13:13:33 +0300

    added the profile customization project git cmd

--- clone.bat
+++ clone.bat
@@ -3,3 +3,4 @@ cd ..
 git clone https://github.com/vilppes/accessibility-resources
 git clone https://github.com/vilppes/notes-on-learning-bluetooth
 git clone https://github.com/vilppes/emergency-messaging --branch dev-sm emergency-messaging-devsm
+git clone https://github.com/vilppes/vilppes
