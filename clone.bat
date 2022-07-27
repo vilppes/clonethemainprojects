74ea0b48d492298cc76691b92249912904e49666
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-27 13:48:36 +0300

    added cboard

--- clone.bat
+++ clone.bat
@@ -4,3 +4,4 @@ git clone https://github.com/vilppes/accessibility-resources
 git clone https://github.com/vilppes/notes-on-learning-bluetooth
 git clone https://github.com/vilppes/emergency-messaging --branch dev-sm emergency-messaging-devsm
 git clone https://github.com/vilppes/vilppes
+git clone --recursive https://github.com/vilppes/cboard.git
