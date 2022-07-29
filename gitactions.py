61f3390e9f8193651fefc9298901214dee16ee86
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-29 08:55:13 +0300

    added functions

--- gitactions.py
+++ gitactions.py
@@ -7,9 +7,11 @@ class GitActionsForMains:
         self.base_git = base_git
     def set_project_names(self,project_gits: list) -> list:
         for pgit in project_gits:
-            pgit1 = pgit.split(self.base_git)[1]
-            pgit2 = pgit1.split(",")
-            self.project_names.append(pgit2[0]+"-"+pgit2[1])
+            self.project_names.append(self.make_a_project_name[0]+"-"+self.make_a_project_name[1])
+    def make_a_project_name(self,project_git: str) -> str:
+        pgit1 = project_git.split(self.base_git)[1]
+        pgit2 = pgit1.split(",")
+        return pgit2
     def get_project_names(self) -> list:
         return self.project_names
     def get_project_gits(self) -> list:
@@ -19,9 +21,23 @@ class GitActionsForMains:
     def get_project_names_without_having_to_call_set_function_separately(self) -> list:
         self.set_project_names_without_having_to_insert_list()
         return self.get_project_names()
-    def git_pull_all_projects(self):
+    def git_pull_all_projects(self) -> None:
         for project_name in self.project_names:
             command = "cd .. & cd "+project_name+" & "+"git pull"
-            command2 = 'cmd /c "{}"'.format(command)
-            os.system(command2)
-    
+            command2 = self.format_command(command)
+            self.run_command(command2)
+    def format_command(self,command):
+        return 'cmd /c "{}"'.format(command)
+    def run_command(self,command):
+        os.system(command)
+    def git_clone_all_projects(self) -> None:
+        for project in self.project_gits:
+            self.git_clone_a_project(self.make_a_project_name(project))
+    def git_clone_a_project(self,pgit2):
+        command = "git clone "
+        if pgit2[2] == "1":
+            command += "--recursive "
+        command += self.base_git + pgit2[0] + " "
+        if pgit2[1] != "main" and pgit2[1] != "master":
+            command += "--branch "+pgit2[1]
+        self.run_command(self.format_command(command))
