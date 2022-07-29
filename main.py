de155f16008e89ca0344273c425b23d8bb287ca9
Pessi Vilppolahti <pessiv@outlook.com>
2022-07-29 11:51:19 +0300

    fixed a typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ gafm = GitActionsForMains([],"https://github.com/vilppes")
 gafm.load_project_gits_from_a_prespecified_file()
 gafm.set_project_names_without_having_to_insert_list()
 while True:
-    i = int(input("what would you like to do? (1 = clone all, 2 = pull all, 3 = exit: "))
+    i = int(input("what would you like to do? (1 = clone all, 2 = pull all, 3 = exit): "))
     if i == 1:
         gafm.git_clone_all_projects()
     elif i == 2:
